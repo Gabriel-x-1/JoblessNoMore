0d501aafcadcfd852913271e7cb1835c2e88d617
Pica Eduard-Ionut <163337754+Pica-Eduard@users.noreply.github.com>
2025-05-05 16:06:46 +0300

    Add files via upload
    Blank project folder

---
+++ Project/src/main/java/com/example/mvcproducts/bootstrap/DataLoader.java
@@ -0,0 +1,18 @@
+package com.example.mvcproducts.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+@Component
+public class DataLoader implements CommandLineRunner {
+
+  public DataLoader() {
+
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+
+  }
+}
